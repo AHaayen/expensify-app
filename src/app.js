371737e23bdfde4e1ae8096f41d1f304723100a7
AndresHaayen <andreshaayen@gmail.com>
2017-12-06 14:25:47 +0100

    Setting up a 404 page :)

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -25,17 +25,25 @@ const EditExpensePage = () => (
 const HelpPage = () => (
     <div>
         This is from my Help Component
+
+    </div>
+);
+
+const NotFoundPage = () => (
+    <div>
+        404!
     </div>
 );
 
 const routes = (
     <BrowserRouter>
-        <div>
+        <Switch>
             <Route path="/" component={ExpenseDashBoardPage} exact={true} />
             <Route path="/create" component={AddExpenseDashBoardPage} />
             <Route path="/edit" component={EditExpensePage} />
             <Route path="/help" component={HelpPage} />
-        </div>
+            <Route component={NotFoundPage} />
+        </Switch>
 
     </BrowserRouter>
 );
