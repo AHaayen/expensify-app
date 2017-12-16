664f393b3b9e3589c1f80459598432933d9a0f25
AndresHaayen <andreshaayen@gmail.com>
2017-12-16 21:50:08 +0100

    Connecting Store and Component with React-Redux

--- src/components/ExpenseDashBoardPage.js
+++ src/components/ExpenseDashBoardPage.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import ExpenseList from './ExpenseList';
 
 const ExpenseDashBoardPage = () => (
     <div>
-        This is from my Dashboard Component
+        <ExpenseList />
     </div>
 );
 
