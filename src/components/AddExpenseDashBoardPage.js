7867a8fe1b32ae7a12e9cf562fb896cc36f9fab3
AndresHaayen <andreshaayen@gmail.com>
2017-12-20 23:50:42 +0100

    Creating Expense Add/Edit Form

--- src/components/AddExpenseDashBoardPage.js
+++ src/components/AddExpenseDashBoardPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import ExpenseForm from './ExpenseForm';
 
 const AddExpenseDashBoardPage = () => (
     <div>
-        This is from my add expense Component
+        <h1>Add Expense</h1>
+        <ExpenseForm />
     </div>
 );
 
