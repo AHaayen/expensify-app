d93d68336196f71b0f5dda3ee717712c4d17e82d
AndresHaayen <andreshaayen@gmail.com>
2018-01-28 19:29:24 +0100

    recapping the whole redux

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -2,50 +2,111 @@
 // Object Destructuring
 //
 
-const person = {
-    name: 'Andres',
-    age: 23,
-    location: {
-        city: 'EARTH',
-        temp: 100
-    }
-};
+// const person = {
+//     name: 'Andres',
+//     age: 23,
+//     location: {
+//         city: 'EARTH',
+//         temp: 12
+//     }
+// };
 
-const {name: firstName = 'Anonymous', age} = person;
-// const name = person.name;
-// const age = person.age;
+// const { name: firstName = 'ANDRES', age } = person;
+// console.log(`${firstName} is ${age}.`);
+
+// const { city, temp: temperature }  = person.location;
+// if (city && temperature) {
+//     console.log(`It's ${temperature} in ${city}`);
+// };
 
 
-console.log(`${firstName} is ${age}`);
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryane Holiday',
+//     publisher: {
+//         // name: 'Penguin'
+//     }
+// };
 
-const {temp: temperature, city} = person.location;
-if (city && temperature) {
-    console.log(`It's ${temperature} in ${city}`)
-};
+// const { name: publisherName = 'Self-Published' } = book.publisher;
 
-const book = {
-    title: 'Ego is the enemy',
-    author: 'Ryan Holiday',
-    publisher: {
-        // name: 'Penguin'
-    }
-};
+// console.log(publisherName);
 
-const {name: publisherName = 'Self-Published'} = book.publisher;
 
-console.log(publisherName);
-
-/*-------------------------------------------------------------------------------*/
 
 //
 // Array Destructuring
 //
 
-const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147'];
+const address = [
+    '12399 S Juniper Street', 
+    'Philadelphia', 
+    'Pennsylvania', 
+    '19147'
+];
 const [, city, state = 'New York'] = address;
-console.log(`You're in ${city} ${state}`)
+console.log(`You are in ${city} ${state}`)
 
-const item = ['Coffee (iced)', '$2.00', '$3.50', '$2.75'];
-const [coffee, , mediumValue] = item;
+const item = ['coffee (hot)', '$2.00', '$2.50', '$2.78'];
 
-console.log(`A medium ${coffee} costs ${mediumValue}`);
+const [coffee, , medium] = item;
+
+console.log(`A medium ${coffee} costs ${medium}`)
+
+
+
+
+
+
+
+// //
+// // Object Destructuring
+// //
+
+// const person = {
+//     name: 'Andres',
+//     age: 23,
+//     location: {
+//         city: 'EARTH',
+//         temp: 100
+//     }
+// };
+
+// const {name: firstName = 'Anonymous', age} = person;
+// // const name = person.name;
+// // const age = person.age;
+
+
+// console.log(`${firstName} is ${age}`);
+
+// const {temp: temperature, city} = person.location;
+// if (city && temperature) {
+//     console.log(`It's ${temperature} in ${city}`)
+// };
+
+// const book = {
+//     title: 'Ego is the enemy',
+//     author: 'Ryan Holiday',
+//     publisher: {
+//         // name: 'Penguin'
+//     }
+// };
+
+// const {name: publisherName = 'Self-Published'} = book.publisher;
+
+// console.log(publisherName);
+
+// /*-------------------------------------------------------------------------------*/
+
+// //
+// // Array Destructuring
+// //
+
+// const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147'];
+// const [, city, state = 'New York'] = address;
+// console.log(`You're in ${city} ${state}`)
+
+// const item = ['Coffee (iced)', '$2.00', '$3.50', '$2.75'];
+// const [coffee, , mediumValue] = item;
+
+// console.log(`A medium ${coffee} costs ${mediumValue}`);
--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -7,26 +7,21 @@ const incrementCount = ({ incrementBy = 1 } = {}) => ({
     incrementBy: incrementBy
 });
 
-const decrementCount = ({decrementBy = 1} = {}) => ({
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
     type: 'DECREMENT',
     decrementBy: decrementBy
 });
 
-const setCount = ({count} = {}) => ({
+const setCount = ({ count } = {}) => ({
     type: 'SET',
     count: count
 });
 
 const resetCount = () => ({
     type: 'RESET'
-});
+})
 
-// Reducers
-// 1. Reducers are pure functions
-// 2. Never change state or action
-
-
-const countReducer = (state = { count: 0 }, action) => {
+const store = createStore((state = { count: 0 }, action) => {
     switch (action.type) {
         case 'INCREMENT':
             return {
@@ -36,39 +31,112 @@ const countReducer = (state = { count: 0 }, action) => {
             return {
                 count: state.count - action.decrementBy
             };
+        case 'RESET':
+            return {
+                count: 0
+            };
         case 'SET':
             return {
                 count: action.count
             }
-        case 'RESET':
-            return {
-                count: 0
-            }
         default:
-            return state
+            return state;
     }
-};
+});
 
-
-const store = createStore(countReducer);
-
+// this function gets called everytime the store changes
 const unsubscribe = store.subscribe(() => {
     console.log(store.getState());
 });
 
-
-// store.dispatch({
-//     type: 'INCREMENT',
-//     incrementBy: 5
-// });
-
-store.dispatch(incrementCount({ incrementBy: 5}));
+store.dispatch(incrementCount({ incrementBy: 5 }));
 
 store.dispatch(incrementCount());
 
 store.dispatch(resetCount());
+
 store.dispatch(decrementCount());
 
-store.dispatch(decrementCount({ decrementBy: 10}));
+store.dispatch(decrementCount({ decrementBy: 10 }));
 
-store.dispatch(setCount({ count: 101 }));
+store.dispatch(setCount({ count: -101 }));
+
+
+
+
+
+
+// import { createStore } from 'redux';
+
+// // Action generators - functions that return action objects
+
+// const incrementCount = ({ incrementBy = 1 } = {}) => ({
+//     type: 'INCREMENT',
+//     incrementBy: incrementBy
+// });
+
+// const decrementCount = ({decrementBy = 1} = {}) => ({
+//     type: 'DECREMENT',
+//     decrementBy: decrementBy
+// });
+
+// const setCount = ({count} = {}) => ({
+//     type: 'SET',
+//     count: count
+// });
+
+// const resetCount = () => ({
+//     type: 'RESET'
+// });
+
+// // Reducers
+// // 1. Reducers are pure functions
+// // 2. Never change state or action
+
+
+// const countReducer = (state = { count: 0 }, action) => {
+//     switch (action.type) {
+//         case 'INCREMENT':
+//             return {
+//                 count: state.count + action.incrementBy
+//             };
+//         case 'DECREMENT':
+//             return {
+//                 count: state.count - action.decrementBy
+//             };
+//         case 'SET':
+//             return {
+//                 count: action.count
+//             }
+//         case 'RESET':
+//             return {
+//                 count: 0
+//             }
+//         default:
+//             return state
+//     }
+// };
+
+
+// const store = createStore(countReducer);
+
+// const unsubscribe = store.subscribe(() => {
+//     console.log(store.getState());
+// });
+
+
+// // store.dispatch({
+// //     type: 'INCREMENT',
+// //     incrementBy: 5
+// // });
+
+// store.dispatch(incrementCount({ incrementBy: 5}));
+
+// store.dispatch(incrementCount());
+
+// store.dispatch(resetCount());
+// store.dispatch(decrementCount());
+
+// store.dispatch(decrementCount({ decrementBy: 10}));
+
+// store.dispatch(setCount({ count: 101 }));
