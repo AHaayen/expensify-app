5cffe37a6fb1f23d84bd1295cdb99b16d0d27ff5
AndresHaayen <andreshaayen@gmail.com>
2018-02-10 21:27:11 +0100

    Learned Mocking Libraries with Jest

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import moment from 'moment';
 import { SingleDatePicker} from 'react-dates';
-import 'react-dates/lib/css/_datepicker.css';
 
-
-// const date = new Date();
-const date = moment();
-console.log(date.format('MMM Do, YYYY'));
 
 export default class ExpenseForm extends React.Component {
     constructor(props) {
